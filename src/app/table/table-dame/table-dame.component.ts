17f22e0d2e12e3de2f9800138619e5edd678edab
vietnvph13644 <vietnvph13644@fpt.edu.vn>
2022-05-13 08:56:51 +0700

    dame,avatar

--- src/app/table/table-dame/table-dame.component.ts
+++ src/app/table/table-dame/table-dame.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-table-dame',
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./table-dame.component.css']
 })
 export class TableDameComponent implements OnInit {
-
-  constructor() { }
+  @Input() dame:number;
+  constructor() {
+    this.dame = 0;
+  }
 
   ngOnInit(): void {
   }
+
+
 
 }
